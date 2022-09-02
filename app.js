f72619fba81b384916347643a3be48c33ba44cd5
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-09-03 03:40:19 +0600

    Image-thumnail-comlete

--- app.js
+++ app.js
@@ -42,6 +42,9 @@ const displayThumnail = singleThumnail  => {
         console.log(thumnail)
         const thumnailDiv = document.createElement('div') ;
         thumnailDiv.classList.add('card');
+        thumnailDiv.classList.add('mb-3');
+        thumnailDiv.classList.add('p-3');
+        
         thumnailDiv.innerHTML=`
             <div class="row g-0">
               <div class="col-md-4">
@@ -50,8 +53,35 @@ const displayThumnail = singleThumnail  => {
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${thumnail.title}</h5>
-                  <p class="card-text">${thumnail.details.slice(0, 200)}</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  <p class="card-text">${thumnail.details.slice(0, 170)}</p>
+                  <p class="card-text">${thumnail.details.slice(0, 120)}</p>
+                <div class="thumnailFlex">
+                        <div>
+                        <img class="authorImg" src="${thumnail.author.img}">
+                        <span class="authorPadding">
+                        <small>${thumnail.author.name}</small><br>
+                        <small class="smallPadding">${thumnail.author.published_date}</small>
+                        </span>
+                        
+                        </div>
+                        <div>
+                            <span><i class="fa-solid fa-eye"></i></span>
+                            <span>${thumnail.total_view}</span>
+                    
+                        </div>
+                        <div>
+                            <span> <i class="fa-regular fa-star"></i></span>
+                            <span> <i class="fa-regular fa-star"></i></span>
+                            <span> <i class="fa-regular fa-star"></i></span>
+                            <span> <i class="fa-regular fa-star"></i></span>
+                        </div>
+
+                        <div>
+                            <span class="icon"><i class="fa-solid fa-arrow-right"></i></span>
+                        </div>
+                    
+                </div>
+                
                 </div>
               </div>
             </div>
