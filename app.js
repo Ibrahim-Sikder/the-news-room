bfd1afe3b0d2a96e1302c9822292c3a1b634734f
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-09-03 07:34:07 +0600

    Modal-complete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const displayCategories = singleCategory => {
         const categoryList = document.createElement('ul');
         categoryList.classList.add('d-flex');
         categoryList.innerHTML=`
-        <li class="category">${category.category_name}</li>
+        <li onclick="loadThumnail()" class="category">${category.category_name}</li>
         
         `
         categoryContainer.appendChild(categoryList)
@@ -33,13 +33,14 @@ const loadThumnail = () =>{
     fetch(`https://openapi.programming-hero.com/api/news/category/01`)
 .then(res => res.json())
 .then(data => displayThumnail(data.data))
+
     
 }
 
 const displayThumnail = singleThumnail  => {
     const thmnailcontainer = document.getElementById('thumnailImg')
     singleThumnail.forEach(thumnail => {
-        console.log(thumnail)
+        //  console.log(thumnail)
         const thumnailDiv = document.createElement('div') ;
         thumnailDiv.classList.add('card');
         thumnailDiv.classList.add('mb-3');
@@ -77,8 +78,12 @@ const displayThumnail = singleThumnail  => {
                         </div>
 
                         <div>
-                            <span class="icon"><i class="fa-solid fa-arrow-right"></i></span>
+                            <span class="icon"><i class="fa-solid fa-arrow-right"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
+                            </span>
+                            
+                            
                         </div>
+                        
                     
                 </div>
                 
@@ -90,9 +95,80 @@ const displayThumnail = singleThumnail  => {
         
         `
         thmnailcontainer.appendChild(thumnailDiv)
-
+       
+        
     });
+   
 
 }
 
+
+
+
+
 loadThumnail();
+
+// display MOdal
+const modalPlay = () =>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+.then(res => res.json())
+.then(data => displayModal(data.data))
+
+    
+}
+
+const displayModal = modal =>{
+   
+    
+    const modalContainer = document.getElementById('staticBackdrop');
+   modal.forEach(singleModal =>{
+    const modalDiv = document.createElement('div');
+    modalDiv.classList.add('modal-dialog');
+    modalDiv.classList.add('modal-lg');
+    modalDiv.innerHTML= `
+    <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <div class="card mb-3">
+        <img  src="${singleModal.thumbnail_url}" class=" modalImg card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${singleModal.title}</h5>
+          <p class="card-text">${singleModal.details}</p>
+          <p class="card-text">${singleModal.details.slice(0, 200)}</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+          </div>
+          <small>${singleModal.total_view}</small>
+          <small>${singleModal.category_id}</small>
+        </div>
+     
+     
+    <div class="modal-footer">
+    <small>${singleModal.author.name}</small>
+    <small>${singleModal.author.published_date}</small>
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+    </div>
+  </div>
+    
+    
+    
+    `
+    modalContainer.appendChild(modalDiv);
+   
+
+    singleModal.slice(0, 1)
+   })
+   
+
+   
+
+}
+
+
+
+
+modalPlay()
